3397c1c71746cd67e55d66a57681614b2ad2f70f
Sarath Ramachandra <sramachandra@conviva.com>
2020-09-30 20:48:05 +0530

    Added source code to use the util

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Test-Repo
-Inserted change in feature branch
+# test-npm-use
+
+$ npm install
+$ node index.js
+
+See the output of the add/sub of two numbers
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const util = require("@SarathRamachandra/test-npm-repo");
+console.log("Addition of two numbers 10 and 5 = "+util.add(10, 5));
+console.log("Subtraction of two numbers 10 and 5 = "+util.sub(10, 5));
