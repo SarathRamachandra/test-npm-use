22b3b01dd0d6c8941a931e3008ab37a706e8e1be
Sarath Ramachandra <sramachandra@conviva.com>
2020-10-01 20:34:16 +0530

    Pushed the changes related to the usage of umd package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 const util = require("@SarathRamachandra/test-npm-repo");
 console.log("Addition of two numbers 10 and 5 = "+util.add(10, 5));
 console.log("Subtraction of two numbers 10 and 5 = "+util.sub(10, 5));
+
+const umd = require("@SarathRamachandra/test-npm-umd");
+console.info("umd = ",umd);
+console.log("umd version = "+umd.Constants.version);
+console.log("umd client fetch indentifier = "+new umd.Client("Test").fetchUniqueNumber());
